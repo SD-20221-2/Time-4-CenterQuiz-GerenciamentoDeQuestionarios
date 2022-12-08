aca31ce086820d45fd01f3c8930d380b78440eaa
Matheus Lázaro <matheuscrf09@discente.ufg.br>
2022-12-08 00:43:34 -0300

    commit

--- Atividades/Unid-II/Lista01Python/Ex1/home01/Salario.py
+++
@@ -1,8 +0,0 @@
-class Salario:
-    def reajustar(cargo, salario):
-        if cargo.lower() == "operador":
-            return salario * 1.2
-        elif (cargo.lower() == "programador") :
-            return salario * 1.18
-        else:
-            return salario
