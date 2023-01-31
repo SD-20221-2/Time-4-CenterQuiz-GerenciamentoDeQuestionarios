d3d2df3853f9eec500f208cf8ae7d1b4e435b08e
matheusLazaroCC-UFG <matheuscrf09@discente.ufg.br>
2023-01-31 16:01:44 -0300

    commit

--- Projeto-CenterQuiz/CenterQuiz/src/main/java/sd/api/rest/controller/GerenciadorCenterQuizController.java
+++ Projeto-CenterQuiz/CenterQuiz/src/main/java/sd/api/rest/controller/GerenciadorCenterQuizController.java
@@ -13,6 +13,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -33,6 +34,7 @@ import sd.api.rest.repository.QuestionarioRepository;
  *
  * @author mathe
  */
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping(value = "/api")
 public class GerenciadorCenterQuizController {
